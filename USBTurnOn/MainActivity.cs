e44940277eb57306bef2fe6f795a0be7482b13ac
zhangs1124 <@example.com>
2026-05-10 05:07:56 +0800

    Simplify app: remove Device Admin, keep only USB debug shortcut

--- USBTurnOn/MainActivity.cs
+++ USBTurnOn/MainActivity.cs
@@ -1,6 +1,6 @@
-using Android.App.Admin;
-using Android.Content;
+﻿using Android.Content;
 using Android.OS;
+using Android.Widget;
 
 namespace USBTurnOn
 {
@@ -11,80 +11,26 @@ namespace USBTurnOn
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            // 閮剔蔭?恍雿?
             SetContentView(Resource.Layout.activity_main);
 
-            // 取得按鈕
+            // ????
             Button button = FindViewById<Button>(Resource.Id.btnToggleUSBDebugging);
-            Button buttonTurnOffScreen = FindViewById<Button>(Resource.Id.buttonTurnOffScreen);
 
-            // 設置按鈕點擊事件
+            // 閮剔蔭??暺?鈭辣嚗?亥歲頧?鈭箏?賊?
             button.Click += (sender, e) =>
             {
-                // 呼叫切換USB偵錯模式的方法
-                ToggleUSBDebugging();
-            };
-
-            buttonTurnOffScreen.Click += (sender, e) =>
-            {
-                // 呼叫關閉螢幕的方法
-                TurnOffScreen();
-            };
-
-
-        }
-
-        private void ToggleUSBDebugging()
-        {
-            // 使用Android的Intent啟動開關USB偵錯模式的設置
-            Intent intent = new Intent(Android.Provider.Settings.ActionApplicationDevelopmentSettings);
-            StartActivity(intent);
-        }
-
-
-        private void TurnOffScreen()
-        {
-            DevicePolicyManager devicePolicyManager = (DevicePolicyManager)GetSystemService(DevicePolicyService);
-            ComponentName adminComponent = new ComponentName(this, Java.Lang.Class.FromType(typeof(MyAdminReceiver)));
-
-            if (!devicePolicyManager.IsAdminActive(adminComponent))
-            {
-                Intent intent = new Intent(DevicePolicyManager.ActionAddDeviceAdmin);
-                intent.PutExtra(DevicePolicyManager.ExtraDeviceAdmin, adminComponent);
-                intent.PutExtra(DevicePolicyManager.ExtraAddExplanation, "Please grant device admin permission to enable this feature.");
-                StartActivityForResult(intent, 1);
-            }
-            else
-            {
-                devicePolicyManager.LockNow();
-            }
-
-        }
-
-
-
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-            if (requestCode == 1)
-            {
-                if (resultCode == Result.Ok)
+                try
                 {
-                    // Device admin permission granted
-                    Toast.MakeText(this, "Device admin permission granted.", ToastLength.Short).Show();
-                    // Lock the screen after permission granted
-                    DevicePolicyManager devicePolicyManager = (DevicePolicyManager)GetSystemService(DevicePolicyService);
-                    ComponentName adminComponent = new ComponentName(this, Java.Lang.Class.FromType(typeof(MyAdminReceiver)));
-                    devicePolicyManager.LockNow();
+                    Intent intent = new Intent(Android.Provider.Settings.ActionApplicationDevelopmentSettings);
+                    intent.AddFlags(ActivityFlags.NewTask);
+                    StartActivity(intent);
                 }
-                else
+                catch (System.Exception ex)
                 {
-                    // Device admin permission not granted
-                    Toast.MakeText(this, "Device admin permission not granted.", ToastLength.Short).Show();
+                    Toast.MakeText(this, "?⊥???閮剖?: " + ex.Message, ToastLength.Long).Show();
                 }
-            }
+            };
         }
-
-
     }
 }
